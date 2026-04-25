a5aa98b88fcc03c18b09af351d84d713a67c048f
EdisonKo <koalli@msn.com>
2026-04-25 12:28:31 +0800

    lean's lede must use luci-theme-argon 18.06 branch

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 清除旧版argon主题并拉取最新版
-rm -rf package/lean/luci-theme-argon && git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # Remove UnblockNeteaseMusicGo upx commands
 sed -i "/upx/d" package/lean/UnblockNeteaseMusicGo/Makefile || true
